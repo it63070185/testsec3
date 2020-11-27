4f17bae4818131d1447301fc956082a73fe86b56
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-27 15:08:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   ?>
   <div class="container mt-5">
     <div class="card-header bg-danger text-dark d-flex justify-content-between">
-      <h3>HOME</h3>
+      <h1>HOME</h1>
        <a href="form.php" class="btn btn-dark">เพิ่ม</a>
     </div>
     <div class="card-body">
