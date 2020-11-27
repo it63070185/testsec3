843032e3e1da237e9e76e3984554db126a69d5fc
it63070185 <73686836+it63070185@users.noreply.github.com>
2020-11-27 14:21:57 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,44 @@
+<?php
+    $conn = mysqli_connect('apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab');
+
+    $id = $_GET['ID'];
+
+    $sql = 'SELECT * FROM testtable WHERE ID = ' . $id . '';
+    $query = mysqli_query($conn, $sql);
+    if (!$query) {
+        header('Location: index.php');
+    } else {
+        $data = mysqli_fetch_assoc($query);
+    }
+    ?>
+  <!DOCTYPE html>
+  <html>
+
+  <head>
+      <title>Comment Form</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  </head>
+
+  <body>
+      <div class="container">
+          <div class="card-header bg-primary text-white d-flex justify-content-between">
+           <h3>EDIT</h3>
+           <a href="index.php" class="btn btn-light">BACK</a>
+          </div>
+          <form action="update.php" method="post" id="CommentForm">
+              <div class="form-group mt-5">
+                <label class="m-3" for="name">Product</label>
+                <input type="text" class="form-control" name="product" id="idName" placeholder="Enter Product">
+                <label class="m-3" for="comment">Price</label>
+                <input type="text" class="form-control" name="price" id="idprice" placeholder="Enter price">
+                <label class="m-3" for="link">Discount</label>
+                <input type="text" class="form-control" name="discount" id="iddiscount" placeholder="Enter Discount">
+                <input class="btn btn-success mt-5" type="submit" id="commentBtn">
+              </div>
+          </form>
+      </div>
+  </body>
+
+  </html>
---
+++ update.php
@@ -0,0 +1,42 @@
+<?php
+	$conn = mysqli_connect('apirat.mysql.database.azure.com', 'it63070185@apirat', 'UEKyfj18', 'ITFlab');
+
+	$product = $_POST['product'];
+    $price = $_POST['price'];
+    $discount = $_POST['discount'];
+    $total = $price*$amount;   
+	$id = $_POST['ID'];
+
+    $sql = 'UPDATE testtable SET product = "'.$product.'", price = "'.$price.'", discount = "'.$discount.'" WHERE ID = '.$id.'';
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5 text-center">
+      
+            <h3>
+                <?php
+                    if (mysqli_query($conn, $sql)) {
+                        echo "Update Completed";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                    mysqli_close($conn);
+                ?>
+            </h3>
+            <a href="index.php" class="btn btn-light mt-3">BACK</a>
+       
+    </div>
+</body>
+
+</html>
